97e8b6adcff4f928050392d262b6bbb18d0d0a77
Michael <mpkelley08@gmail.com>
2018-01-29 12:15:03 -0500

    changes from mentor meeting added

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -13,7 +13,7 @@ class ChargesController < ApplicationController
       charge = Stripe::Charge.create(
         customer: customer.id, # Note -- this is NOT the user_id in your app
         amount: 1500, #Amount.default,
-        description: "BigMoney Membership - #{current_user.email}",
+        description: "Premium Membership - #{current_user.email}",
         currency: 'usd'
       )
 
